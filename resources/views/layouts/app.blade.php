c5ff58020ae16bbbddbeaaa45aec263787288e24
ngoc tho <ngoctho.t1k54@gmail.com>
2020-11-12 16:33:39 +0700

    create layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,88 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>@yield('title')</title>
+    <meta name="description" content="Sufee Admin - HTML5 Admin Template">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="apple-touch-icon" href="apple-icon.png">
+    <link rel="shortcut icon" href="favicon.ico">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="stylesheet" href="vendor/bower-hkdt/vendors/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendor/bower-hkdt/vendors/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendor/bower-hkdt/vendors/themify-icons/css/themify-icons.css">
+    <link rel="stylesheet" href="vendor/bower-hkdt/vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="vendor/bower-hkdt/vendors/selectFX/css/cs-skin-elastic.css">
+    <link rel="stylesheet" href="vendor/bower-hkdt/vendors/jqvmap/dist/jqvmap.min.css">
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="vendor/bower-hkdt/assets/css/style.css">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+
 </head>
+
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
 
-                    </ul>
+<!-- Left Panel -->
+@include('layouts.left_panel')
+<!-- Left Panel -->
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+<!-- Right Panel -->
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+<div id="right-panel" class="right-panel">
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+    <!-- Header-->
+    @include('layouts.header')
+    <!-- Header-->
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+    <div class="content mt-3">
+        @yield('content')
+    </div> <!-- .content -->
+</div><!-- /#right-panel -->
+
+<!-- Right Panel -->
+
+<script src="vendor/bower-hkdt/vendors/jquery/dist/jquery.min.js"></script>
+<script src="vendor/bower-hkdt/vendors/popper.js/dist/umd/popper.min.js"></script>
+<script src="vendor/bower-hkdt/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="vendor/bower-hkdt/assets/js/main.js"></script>
+
+
+<script src="vendor/bower-hkdt/vendors/chart.js/dist/Chart.bundle.min.js"></script>
+<script src="vendor/bower-hkdt/assets/js/dashboard.js"></script>
+<script src="vendor/bower-hkdt/assets/js/widgets.js"></script>
+<script src="vendor/bower-hkdt/vendors/jqvmap/dist/jquery.vmap.min.js"></script>
+<script src="vendor/bower-hkdt/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+<script src="vendor/bower-hkdt/vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+<script>
+    (function($) {
+        "use strict";
+
+        jQuery('#vmap').vectorMap({
+            map: 'world_en',
+            backgroundColor: null,
+            color: '#ffffff',
+            hoverOpacity: 0.7,
+            selectedColor: '#1de9b6',
+            enableZoom: true,
+            showTooltip: true,
+            values: sample_data,
+            scaleColors: ['#1de9b6', '#03a9f5'],
+            normalizeFunction: 'polynomial'
+        });
+    })(jQuery);
+</script>
+
 </body>
+
 </html>
